391c06a04834a8db38e0a07cb71d5cefe2fca7c9
Thiagoferreira13 <thferreira147@gmail.com>
2024-10-26 19:35:47 -0300

    Criando a casse D com atributos e metodos iniciais

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public String getD1() {
+        return D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD1(String novoD1) {
+        D1 = novoD1;
+    }
+
+    public void setD2(int novoD2) {
+        D2 = novoD2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1" + "É um metodo da Classe D");
+    }
+
+    public void MD2() {
+        System.out.println("MD2" + "É um metodo da Classe D");
+   }
+}
