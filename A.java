c24976a3e47f94a39b2d3806d01f101d3cb30661
Thiagoferreira13 <thferreira147@gmail.com>
2024-10-25 23:36:48 -0300

    Adicionando as classes A e B com metodos e atrivutos

---
+++ A.java
@@ -0,0 +1,43 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+
+    public int getA1() {
+
+        return A1;
+
+    }
+
+    public float getA2() {
+
+        return A2;
+
+    }
+
+    public void setA1(int novoValor) {
+
+        A1 = novoValor;
+
+    }
+
+    public void setA2(int novoValor) {
+
+        A2 = novoValor;
+
+    }
+
+    public void MA1() {
+        
+        System.out.println("MA1");
+
+    }
+
+    public void MA2() {
+        
+        System.out.println("MA2");
+
+    }
+
+}
