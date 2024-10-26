84ccc4f666b4c08f43c1d7a9598d346d8ab64909
Thiagoferreira13 <thferreira147@gmail.com>
2024-10-26 20:36:28 -0300

    Resolvendo problema de conflito entre os metodos MD3 e MD4

--- D.java
+++ D.java
@@ -24,7 +24,14 @@ public class D {
 
     public void MD2() {
         System.out.println("MD2" + "É um metodo da Classe D");
-   }
+    }
+    
+    public void MD3() {
+        System.out.println("MD3");
+    }
 
-   
+    public void MD4() {
+        System.out.println("MD4");  
+    }
+
 }
